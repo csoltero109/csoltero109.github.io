8a82dfe96e125e5c22e1ec9bd7b2999709ce0a85
csoltero109 <chrissoltero109@gmail.com>
2018-08-30 21:29:01 -0700

    updated favicon and jobs

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ var paragraph = {
 	text:'Managed the development of an application that detected duplicate employee accounts across multiple databases. Researched, identified, and referred an alternative course of action as opposed to original approach to project.  Configuring layout of applications and migrating data for the Identity and Access Management (IAM) department.  Wrote up a document that lays out machine learning concepts and how it can be applied in the workplace. This document references practices utilizing open source APIs from Google such as TensorFlow, TensorBoard, and Scikit Learn in combination with Python. Researching and developing a blockchain app to determine the pros and cons for implementing in the company.'
 	}
 
+
+var paragraphSJ = {
+  company: 'Saint Jospeh High School',
+  position: 'IT Assistant',
+  location: 'Lakewood, CA',
+  workPeriod: 'April 2017 - May 2018',
+  text:'Configuring, implementing, and integrating commercial off-the-shelf (COTS) software such as Dell SonicWALL and AirWatch. These help in managing over 400 devices for students and staff members on the school campus. Participates in Technology department meetings concerning issues with our worksite server, projects, operating systems, and back-end software. Researches, identifies, and refers complex technical problems related to platform management, network connectivity, and desktop support. Successfully configured Google Sheets API with Google Forms to create a ticket request program for school faculty members to communicate with supporting staff. This project was aimed to discourage the waste of paper forms.'
+  }
+
+var paragraphNASA = {
+  company: 'NASA Robotics Programmer (Swarmathon)',
+  position: 'Robot Programmer',
+  location: 'Los Angeles, CA',
+  workPeriod: 'August 2017 - May 2018',
+  text:'Utilizing ROS framework to improve the mapping procedures on the Swarmathon rovers provided by NASA. Building a simulated Grid-Map of a rover\'s surroundings with the published sonar and odometry data. The simulation was made possible by integrating the ROS package rviz. The project was developed on Ubuntu 16.04 with the programming language being C++.'
+  }
+
+
+
+
 Vue.component('paragraph', {
   template: '<p>' + paragraph + '</p>'
 });
@@ -48,7 +68,9 @@ Vue.component('display', {
 			</div>
 		</div>
 	</div>
+  <br>
   </div>
+
   `
 });
 
@@ -58,7 +80,10 @@ var app4 = new Vue({
   el: '#paragraphs',
   data: {
     posts: [
-      { id: 1, corp: paragraph.company, pos: paragraph.position, loc: paragraph.location, period: paragraph.workPeriod, details:paragraph.text }
+      { id: 1, corp: paragraph.company, pos: paragraph.position, loc: paragraph.location, period: paragraph.workPeriod, details:paragraph.text },
+      { id: 2, corp: paragraphNASA.company, pos: paragraphNASA.position, loc: paragraphNASA.location, period: paragraphNASA.workPeriod, details:paragraphNASA.text },
+      { id: 3, corp: paragraphSJ.company, pos: paragraphSJ.position, loc: paragraphSJ.location, period: paragraphSJ.workPeriod, details:paragraphSJ.text }
+      
     ]
   }
 })
